7b97304a9e5d3bfdd9609eeb90228bb5cf81c40e
realpearson <60543594+realpearson@users.noreply.github.com>
2022-04-17 15:08:24 +0200

    Updated sketch

--- sketch.js
+++ sketch.js
@@ -33,7 +33,6 @@ function draw() {
   //for(let g of grids) g.display();
   //translate(-width/4+(rows-1)*size/2,-width/4 + (cols-1)*size/2);
   translate(-400 +(rows-1)*size/2,-400 +(cols-1)*size/2);
-  //scale(2,2);
   grids[0].display();
   rotate(rot);
   grids[1].display();
@@ -45,20 +44,47 @@ function draw() {
 
 function grid(){
   let pg = createGraphics(size*(cols-1), size*(rows-1));
-  //pg.background(random(255),random(255),random(255), 50);
   
-  function setup(){
-    pg.stroke(random(255), random(255), random(255));
-    for(let x = 0; x < cols; x++){
-      for(let y = 0; y < rows; y++){
-        pg.line(x * size, 0, x * size, (rows-1)*size);
-        pg.line(0, y * size, (cols-1)*size, y * size);
-      }
+  //Offset Logic
+  let seedX = random(1000);
+  let seedY = random(1000);
+  let seedZoom = random(1000);
+  const inc = 0.00123;
+  const offsetAmt = size / 2;
+  
+  pg.stroke(random(255), random(255), random(255));
+  pg.strokeWeight(3);
+  for(let x = 0; x < cols; x++){
+    for(let y = 0; y < rows; y++){
+      pg.line(x * size, 0, x * size, (rows-1)*size);
+      pg.line(0, y * size, (cols-1)*size, y * size);
     }
   }
-  setup();
+  
   
   this.display = function(){
-    image(pg, -(rows-1)*size/2, -(cols-1)*size/2);
+    if(random(100) < 5){
+      pg.erase();
+      const rx = Math.floor(random(cols));
+      const ry = Math.floor(random(rows));
+      pg.strokeWeight(4);
+      pg.rect(rx * size, ry * size, size, size);
+      pg.noErase();
+    } else if(random(100) < 2) {
+        const rx = Math.floor(random(cols));
+        const ry = Math.floor(random(rows));
+        //pg.strokeWeight(4);
+        pg.fill(random(255), random(255), random(255));
+      
+        pg.rect(rx * size, ry * size, size, size);
+    }
+    seedX += inc;
+    seedY += inc;
+    seedZoom += inc;
+    const offX = (noise(seedX) - 0.5) * 2 * offsetAmt;
+    const offY = (noise(seedY) - 0.5) * 2 * offsetAmt;
+    const zoom = noise(seedZoom) +1;
+    scale(zoom, zoom);
+    image(pg, -(rows-1)*size/2 + offX, -(cols-1)*size/2 + offY);
   }
 }
